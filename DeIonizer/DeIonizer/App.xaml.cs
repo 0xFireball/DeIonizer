9ce979a67eb385d66ef7df1362a456642d33518b
0xFireball <0xFireball@outlook.com>
2016-11-09 14:19:53 -0800

    add avalonia platforms

---
+++ DeIonizer/DeIonizer/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Diagnostics;
+using Avalonia.Logging.Serilog;
+using Avalonia.Markup.Xaml;
+using Serilog;
+
+namespace DeIonizer
+{
+    internal class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+            base.Initialize();
+        }
+
+        private static void Main(string[] args)
+        {
+            InitializeLogging();
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+
+        public static void AttachDevTools(Window window)
+        {
+#if DEBUG
+            DevTools.Attach(window);
+#endif
+        }
+
+        private static void InitializeLogging()
+        {
+#if DEBUG
+
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+
+#endif
+        }
+    }
+}
